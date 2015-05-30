5e35ab9f190d2bffb40679a8ba66fdc3339e804f
Sunshine Yang <sunshineyang@Sunshines-MacBook-Pro.local>
2015-05-30 20:54:24 +0200

    Initial Commit

---
+++ Ribbit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Ribbit
+//
+//  Created by Sunshine Yang on 5/30/15.
+//  Copyright (c) 2015 SunshineYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
